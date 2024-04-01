54bbb4fec112f2414c98f5960dd0afdfa27e36fa
mori-goq <mori@goq.co.jp>
2024-04-01 17:58:55 +0900

    レスポンスのkanaの型を削除

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,8 @@
 export type response = {
   address: string;
-  address_kana: string;
   city: string;
-  city_kana: string;
   pref: string;
-  pref_kana: string;
   town: string;
-  town_kana: string;
   zipcode: string;
 };
 
