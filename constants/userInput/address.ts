ae49549f52404e50789dc557fe3a61669d358864
marumoto <marumoto@goq.co.jp>
2024-04-15 11:50:45 +0900

    テスト用のstubデータを追加

---
+++ constants/userInput/address.ts
@@ -0,0 +1,3 @@
+export const partInputAddress = '宇品';
+
+export const fullInputAddress = '広島県広島市南区丹那町';
---
+++ constants/userInput/zipcode.ts
@@ -0,0 +1,5 @@
+const partInputZipcode = '734';
+
+const fullinputZipcodeIncludingHyphen = '732-0021';
+
+const fullinputZipcodeExcludingHyphen = '7320021';
