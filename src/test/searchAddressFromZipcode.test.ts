ef31a882c20b97847a1a6caa178975e88b29f6d5
marumoto <marumoto@goq.co.jp>
2024-04-16 18:41:59 +0900

    テストに名前をつける

--- src/test/searchAddressFromZipcode.test.ts
+++ src/test/searchAddressFromZipcode.test.ts
@@ -1,3 +1,7 @@
 import { expect } from '@jest/globals';
 import { addresses } from '../../constants/masterData/address';
 import { fullinputZipcodeExcludingHyphen } from '../../constants/userInput/zipcode';
+
+test('ユーザー入力から取得した郵便番号を元に、完全一致で郵便番号から住所を検索する', () => {
+  
+});
