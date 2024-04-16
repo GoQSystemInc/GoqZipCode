479f41377b9155026056fc0a26e7adebfe1691a9
marumoto <marumoto@goq.co.jp>
2024-04-16 18:42:29 +0900

    期待するデータを用意

--- src/test/searchAddressFromZipcode.test.ts
+++ src/test/searchAddressFromZipcode.test.ts
@@ -3,5 +3,10 @@ import { addresses } from '../../constants/masterData/address';
 import { fullinputZipcodeExcludingHyphen } from '../../constants/userInput/zipcode';
 
 test('ユーザー入力から取得した郵便番号を元に、完全一致で郵便番号から住所を検索する', () => {
-  
+  const expectedData = {
+    zipcode: '7320021',
+    pref: '広島県',
+    city: '広島市東区',
+    town: '中山新町',
+  };
 });
