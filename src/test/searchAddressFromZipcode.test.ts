7af753ea9fcd55d296ba56812c985d1f0d826ed1
marumoto <marumoto@goq.co.jp>
2024-04-17 10:12:46 +0900

    ハイフンありのテストデータも用意

--- src/test/searchAddressFromZipcode.test.ts
+++ src/test/searchAddressFromZipcode.test.ts
@@ -3,8 +3,10 @@ import { addresses } from '../../constants/masterData/address';
 import { fullinputZipcodeExcludingHyphen } from '../../constants/userInput/zipcode';
 
 test('ユーザー入力から取得した郵便番号の全角数字を半角に変換 ハイフンが入っていても数字のみの抽出', () => {
-  const testData = '７３２００２１';
-  const expectedData = '7320021';
+  const testDataExcludingHyphen = '７３２００２１';
+  const testDataIncludingHyphen = '７３２ー００２１';
+  const expectedDataExcludingHyphen = '7320021';
+  const expectedDataIncludingHyphen = '732ー0021';
   const a: string = testData.replace(/[０-９]/g, (s: string) =>
     String.fromCharCode(s.charCodeAt(0) - 65248)
   );
@@ -27,3 +29,11 @@ test('ユーザー入力から取得した郵便番号を元に、完全一致�
 
   expect(matchAddress).toEqual(expectedData);
 });
+
+// TODO:非同期でrejectされた時のテストどうやって書くか検討がつかいないので、一旦保留
+// test('ユーザーから受け取ったデータがマスタデータと合致しない場合はアラート出力', () => {
+//   const matchAddress = undefined;
+//   if (matchAddress === undefined) {
+//     return expect('指定の郵便番号に一致する住所は見つかりませんでした').rejects.toMatch('指定の郵便番号に一致する住所は見つかりませんでした');;
+//   }
+// })
