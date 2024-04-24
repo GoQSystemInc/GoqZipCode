8bc31cbaacf3496ceb3b19c2ed9ec1821098ff2d
marumoto <marumoto@goq.co.jp>
2024-04-24 20:56:50 +0900

    review-fix:テストの順番をテストの種類よって分ける

--- src/test/searchAddressFromZipcode.test.ts
+++ src/test/searchAddressFromZipcode.test.ts
@@ -75,12 +75,12 @@ describe('郵便番号が期待する桁数かチェック', () => {
     expect(goqZipCode.checkLength(true, 7)).toBe(true);
   });
 
-  test('部分一致検索で入力データが2文字以上ならtrueを返す', () => {
-    expect(goqZipCode.checkLength(false, 2)).toBe(true);
-  });
-
   test('完全一致検索で入力データが7文字以外ならfalseを返す', () => {
     expect(goqZipCode.checkLength(true, 8)).toBe(false);
+  });
+
+  test('部分一致検索で入力データが2文字以上ならtrueを返す', () => {
+    expect(goqZipCode.checkLength(false, 2)).toBe(true);
   });
 
   test('部分一致検索で入力データが1文字以下の場合はfalseを返す', () => {
