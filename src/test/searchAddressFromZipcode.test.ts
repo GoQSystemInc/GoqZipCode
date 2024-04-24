6542c8af9572920c74203ebe9be5d3b9f046dc07
marumoto <marumoto@goq.co.jp>
2024-04-24 21:12:26 +0900

    review-fix:半角数字以外を除く処理をreplaceAllで実装

--- src/test/searchAddressFromZipcode.test.ts
+++ src/test/searchAddressFromZipcode.test.ts
@@ -13,8 +13,9 @@ const goqZipCode = {
     const a: string = testZipCode.replace(/[０-９]/g, (s: string) =>
       String.fromCharCode(s.charCodeAt(0) - 65248)
     );
-    const b: RegExpMatchArray | [] = a.match(/\d/g) || [];
-    return b.join('');
+    
+    const b = a.replaceAll(/\D/g, '')
+    return b;
   },
 
   checkLength: function (isExact: boolean, length: number) {
