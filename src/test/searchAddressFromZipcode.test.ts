da35850a6e99ddc7074014a04abe924a2a9ff79f
marumoto <marumoto@goq.co.jp>
2024-04-25 14:48:19 +0900

    review-fix:作成中のため今回のプルリクでは一旦削除

--- src/test/searchAddressFromZipcode.test.ts
+++ src/test/searchAddressFromZipcode.test.ts
@@ -104,18 +104,3 @@ describe('オプションによってハイフンを付与', () => {
     );
   });
 });
-
-test('ユーザー入力から取得した郵便番号を元に、完全一致で郵便番号から住所を検索する', () => {
-  const expectedAddressData = {
-    zipcode: '7320021',
-    pref: '広島県',
-    city: '広島市東区',
-    town: '中山新町',
-  };
-
-  const matchAddress = addresses.find(
-    (address) => address.zipcode === fullInputZipCodeExcludingHyphen
-  );
-
-  expect(matchAddress).toEqual(expectedAddressData);
-});
