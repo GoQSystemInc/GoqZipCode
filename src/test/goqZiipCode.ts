ecaa8dd7ca2b1fb38b19c9f3c18cfa24a6b97bab
marumoto <marumoto@goq.co.jp>
2024-04-26 21:46:22 +0900

    refactor:lint実行

--- src/test/goqZiipCode.ts
+++ src/test/goqZiipCode.ts
@@ -2,11 +2,15 @@ import type { Address } from './type';
 
 export const goqZipCode = {
   convertZipCode: function (testZipCode: string): string {
-    const halfWidthZipCode: string = testZipCode.replace(/[０-９]/g, (s: string) =>
-      String.fromCharCode(s.charCodeAt(0) - 65248)
+    const halfWidthZipCode: string = testZipCode.replace(
+      /[０-９]/g,
+      (s: string) => String.fromCharCode(s.charCodeAt(0) - 65248)
     );
 
-    const halfWidthZipCodeExcludingHyphen = halfWidthZipCode.replaceAll(/\D/g, '');
+    const halfWidthZipCodeExcludingHyphen = halfWidthZipCode.replaceAll(
+      /\D/g,
+      ''
+    );
     return halfWidthZipCodeExcludingHyphen;
   },
 
