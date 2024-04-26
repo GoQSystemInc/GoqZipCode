8d5cf99079a76004bf12dfc3bf66f084777e8789
marumoto <marumoto@goq.co.jp>
2024-04-26 21:26:08 +0900

    feature:作成したテストケースをテストスイートしてまとめる

--- src/test/searchAddressFromZipCode.test.ts
+++ src/test/searchAddressFromZipCode.test.ts
@@ -76,14 +76,23 @@ describe('オプションによってハイフンを付与', () => {
   });
 });
 
-test('郵便番号で検索して住所データを取得', () => {
-  const expectedAddressData = {
-    zipcode: '7340001',
-    pref: '広島県',
-    city: '広島市南区',
-    town: '出汐',
-  };
+describe('郵便番号で住所データ検索', () => {
+  test('郵便番号で検索して住所データを取得', () => {
+    const expectedAddressData = {
+      zipcode: '7340001',
+      pref: '広島県',
+      city: '広島市南区',
+      town: '出汐',
+    };
 
-  expect(goqZipCode.searchMachingZipCode(addresses, '7340001')).toBe(expectedAddressData);
-  expect(goqZipCode.searchMachingZipCode(addresses, '7330001')).toBe( '指定の郵便番号に一致する住所は見つかりませんでした');
+    expect(goqZipCode.searchMachingZipCode(addresses, '7340001')).toBe(
+      expectedAddressData
+    );
+  });
+
+  test('郵便番号検索で一致するデータがない旨メッセージ表示', () => {
+    expect(goqZipCode.searchMachingZipCode(addresses, '7330001')).toBe(
+      '指定の郵便番号に一致する住所は見つかりませんでした'
+    );
+  });
 });
