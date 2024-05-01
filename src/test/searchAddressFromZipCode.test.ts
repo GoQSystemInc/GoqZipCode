50b985d443e0ef4f71f3b5bafa3f645ffb34b048
marumoto <marumoto@goq.co.jp>
2024-05-01 17:09:01 +0900

    convertZipCodeを外部へ切り出し

--- src/test/searchAddressFromZipCode.test.ts
+++ src/test/searchAddressFromZipCode.test.ts
@@ -4,34 +4,6 @@ import { addresses } from './constants/address';
 import { addressExcludingHyphenDataList } from './constants';
 import { addressIncludingHyphenDataList } from './constants';
 
-
-describe('convertZipCodeの動作をテスト', () => {
-  const expectedZipCodeExcludingHyphen = '7320021';
-  test('全角の郵便番号を半角に変換', () => {
-    expect(goqZipCode.convertZipCode('７３２００２１')).toBe(
-      expectedZipCodeExcludingHyphen
-    );
-  });
-
-  test('全角の郵便番号を半角に変換して、ハイフンを削除', () => {
-    expect(goqZipCode.convertZipCode('７３２ー００２１')).toBe(
-      expectedZipCodeExcludingHyphen
-    );
-  });
-
-  test('半角のハイフンを削除', () => {
-    expect(goqZipCode.convertZipCode('732-0021')).toBe(
-      expectedZipCodeExcludingHyphen
-    );
-  });
-
-  test('半角と全角の郵便番号を半角に変換', () => {
-    expect(goqZipCode.convertZipCode('７３2００２1')).toBe(
-      expectedZipCodeExcludingHyphen
-    );
-  });
-});
-
 describe('郵便番号が期待する桁数かチェック', () => {
   test('完全一致検索で入力データが7文字ならtrueを返す', () => {
     expect(goqZipCode.checkLength(true, 7)).toBe(true);
