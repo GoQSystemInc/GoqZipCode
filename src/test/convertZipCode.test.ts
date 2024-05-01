13b3d230320d0e7ef53e4433dd8d18827f6fd39f
marumoto <marumoto@goq.co.jp>
2024-05-01 19:21:35 +0900

    review-fix:test関数前には空行を

--- src/test/convertZipCode.test.ts
+++ src/test/convertZipCode.test.ts
@@ -3,6 +3,7 @@ import { goqZipCode } from './utils/goqZipCode';
 
 describe('convertZipCodeの動作をテスト', () => {
   const expectedZipCodeExcludingHyphen = '7320021';
+  
   test('全角の郵便番号を半角に変換', () => {
     expect(goqZipCode.convertZipCode('７３２００２１')).toBe(
       expectedZipCodeExcludingHyphen
