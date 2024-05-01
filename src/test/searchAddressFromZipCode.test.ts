af59e6ba7c952f4dad8eb7efe3fcda71161eccad
marumoto <marumoto@goq.co.jp>
2024-05-01 17:10:35 +0900

    checkLengthのテストを外部へ切り出し

--- src/test/searchAddressFromZipCode.test.ts
+++ src/test/searchAddressFromZipCode.test.ts
@@ -4,24 +4,6 @@ import { addresses } from './constants/address';
 import { addressExcludingHyphenDataList } from './constants';
 import { addressIncludingHyphenDataList } from './constants';
 
-describe('郵便番号が期待する桁数かチェック', () => {
-  test('完全一致検索で入力データが7文字ならtrueを返す', () => {
-    expect(goqZipCode.checkLength(true, 7)).toBe(true);
-  });
-
-  test('完全一致検索で入力データが7文字以外ならfalseを返す', () => {
-    expect(goqZipCode.checkLength(true, 8)).toBe(false);
-  });
-
-  test('部分一致検索で入力データが2文字以上ならtrueを返す', () => {
-    expect(goqZipCode.checkLength(false, 2)).toBe(true);
-  });
-
-  test('部分一致検索で入力データが1文字以下の場合はfalseを返す', () => {
-    expect(goqZipCode.checkLength(false, 1)).toBe(false);
-  });
-});
-
 describe('オプションによってハイフンを付与', () => {
   test('オプションでハイフンありを指定している場合、郵便番号にハイフンを追加する', () => {
     expect(goqZipCode.convertHyphenatedZipCode(true, addresses)).toEqual(
