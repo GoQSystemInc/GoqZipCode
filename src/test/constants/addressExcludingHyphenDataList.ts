4714b3328047bc1e5915ea10f6f8768a0d1d6a41
marumoto <marumoto@goq.co.jp>
2024-05-01 17:14:05 +0900

    review-fix:convertZipCodeのテストファイルを外部へ切り出し

--- src/test/constants/addressExcludingHyphenDataList.ts
+++ src/test/constants/addressExcludingHyphenDataList.ts
@@ -1,57 +1,56 @@
-
 export const addressExcludingHyphenDataList = [
-	{
-		zipcode: '7340001',
-		pref: '広島県',
-		city: '広島市南区',
-		town: '出汐',
-	},
-	{
-		zipcode: '7340002',
-		pref: '広島県',
-		city: '広島市南区',
-		town: '西旭町',
-	},
-	{
-		zipcode: '7340003',
-		pref: '広島県',
-		city: '広島市南区',
-		town: '宇品東',
-	},
-	{
-		zipcode: '7340004',
-		pref: '広島県',
-		city: '広島市南区',
-		town: '宇品神田',
-	},
-	{
-		zipcode: '7340005',
-		pref: '広島県',
-		city: '広島市南区',
-		town: '翠',
-	},
-	{
-		zipcode: '7320021',
-		pref: '広島県',
-		city: '広島市東区',
-		town: '中山新町',
-	},
-	{
-		zipcode: '7320057',
-		pref: '広島県',
-		city: '広島市東区',
-		town: '二葉の里',
-	},
-	{
-		zipcode: '7320061',
-		pref: '広島県',
-		city: '広島市東区',
-		town: '牛田山',
-	},
-	{
-		zipcode: '7320062',
-		pref: '広島県',
-		city: '広島市東区',
-		town: '牛田早稲田',
-	},
+  {
+    zipcode: '7340001',
+    pref: '広島県',
+    city: '広島市南区',
+    town: '出汐',
+  },
+  {
+    zipcode: '7340002',
+    pref: '広島県',
+    city: '広島市南区',
+    town: '西旭町',
+  },
+  {
+    zipcode: '7340003',
+    pref: '広島県',
+    city: '広島市南区',
+    town: '宇品東',
+  },
+  {
+    zipcode: '7340004',
+    pref: '広島県',
+    city: '広島市南区',
+    town: '宇品神田',
+  },
+  {
+    zipcode: '7340005',
+    pref: '広島県',
+    city: '広島市南区',
+    town: '翠',
+  },
+  {
+    zipcode: '7320021',
+    pref: '広島県',
+    city: '広島市東区',
+    town: '中山新町',
+  },
+  {
+    zipcode: '7320057',
+    pref: '広島県',
+    city: '広島市東区',
+    town: '二葉の里',
+  },
+  {
+    zipcode: '7320061',
+    pref: '広島県',
+    city: '広島市東区',
+    town: '牛田山',
+  },
+  {
+    zipcode: '7320062',
+    pref: '広島県',
+    city: '広島市東区',
+    town: '牛田早稲田',
+  },
 ];
