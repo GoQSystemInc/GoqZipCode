9f2b01648254103e4eca43ed6f3e01a500b92939
marumoto <marumoto@goq.co.jp>
2024-05-02 15:11:00 +0900

    review-fixstringの比較にtoEqualを使用しない

--- src/test/convertHyphenatedZipCode.test.ts
+++ src/test/convertHyphenatedZipCode.test.ts
@@ -3,6 +3,6 @@ import { goqZipCode } from './goqZipCode';
 
 describe('オプションによってハイフンを付与', () => {
   test('オプションでハイフンありを指定している場合、郵便番号にハイフンを追加する', () => {
-    expect(goqZipCode.convertHyphenatedZipCode('7340001')).toEqual('734-0001');
+    expect(goqZipCode.convertHyphenatedZipCode('7340001')).toBe('734-0001');
   });
 });
