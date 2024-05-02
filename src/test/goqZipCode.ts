3a47a2f3f10490b0518694f5b6140e53eba95ae9
marumoto <marumoto@goq.co.jp>
2024-05-02 10:40:57 +0900

    refactor:関数を外部へ切り出したためimportした関数を使ってオブジェクト内に定義

--- src/test/goqZipCode.ts
+++ src/test/goqZipCode.ts
@@ -1,35 +1,7 @@
-import type { HyphenatedZipCode } from './type/type';
+import { checkLength, convertHyphenatedZipCode, convertZipCode  } from './utils'
 
 export const goqZipCode = {
-  convertZipCode: function (testZipCode: string): string {
-    const halfWidthZipCode: string = testZipCode.replace(
-      /[０-９]/g,
-      (s: string) => String.fromCharCode(s.charCodeAt(0) - 65248)
-    );
-
-    const halfWidthZipCodeExcludingHyphen = halfWidthZipCode.replaceAll(
-      /\D/g,
-      ''
-    );
-
-    return halfWidthZipCodeExcludingHyphen;
-  },
-
-  checkLength: function (isExact: boolean, length: number): boolean {
-    if (isExact === true && length === 7) {
-      return true;
-    }
-
-    if (isExact === false && length >= 2 && length <= 7) {
-      return true;
-    }
-
-    return false;
-  },
-
-  convertHyphenatedZipCode: function (
-    zipCode: string | HyphenatedZipCode
-  ): HyphenatedZipCode | string {
-    return `${zipCode.slice(0, 3)}-${zipCode.slice(3)}`;
-  },
+  convertZipCode,
+  checkLength,
+  convertHyphenatedZipCode,
 };
