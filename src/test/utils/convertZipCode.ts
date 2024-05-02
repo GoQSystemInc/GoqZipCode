d92b8ba07ff2eaa4795235b4098608df020ef9fe
marumoto <marumoto@goq.co.jp>
2024-05-02 15:33:47 +0900

    lint

--- src/test/utils/convertZipCode.ts
+++ src/test/utils/convertZipCode.ts
@@ -1,7 +1,6 @@
 export const convertZipCode = (zipCode: string): string => {
-  const halfWidthZipCode: string = zipCode.replace(
-    /[０-９]/g,
-    (s: string) => String.fromCharCode(s.charCodeAt(0) - 65248)
+  const halfWidthZipCode: string = zipCode.replace(/[０-９]/g, (s: string) =>
+    String.fromCharCode(s.charCodeAt(0) - 65248)
   );
 
   const halfWidthZipCodeExcludingHyphen = halfWidthZipCode.replaceAll(
