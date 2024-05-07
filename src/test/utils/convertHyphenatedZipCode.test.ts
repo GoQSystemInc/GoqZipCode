383a62537e429a92aff607739865766a8e7fe9a6
marumoto <marumoto@goq.co.jp>
2024-05-07 11:15:21 +0900

    review-fix:関数をそのままimportする

--- src/test/utils/convertHyphenatedZipCode.test.ts
+++ src/test/utils/convertHyphenatedZipCode.test.ts
@@ -1,6 +1,6 @@
 import { expect } from '@jest/globals';
-import { goqZipCode } from '../goqZipCode';
+import { convertHyphenatedZipCode } from './';
 
 test('郵便番号にハイフンを追加', () => {
-  expect(goqZipCode.convertHyphenatedZipCode('7340001')).toBe('734-0001');
+  expect(convertHyphenatedZipCode('7340001')).toBe('734-0001');
 });
--- src/test/utils/convertZipCode.test.ts
+++ src/test/utils/convertZipCode.test.ts
@@ -1,29 +1,29 @@
 import { expect } from '@jest/globals';
-import { goqZipCode } from '../goqZipCode';
+import { convertZipCode } from './';
 
 describe('convertZipCodeの動作をテスト', () => {
   const expectedZipCodeExcludingHyphen = '7320021';
 
   test('全角の郵便番号を半角に変換', () => {
-    expect(goqZipCode.convertZipCode('７３２００２１')).toBe(
+    expect(convertZipCode('７３２００２１')).toBe(
       expectedZipCodeExcludingHyphen
     );
   });
 
   test('全角の郵便番号を半角に変換して、ハイフンを削除', () => {
-    expect(goqZipCode.convertZipCode('７３２ー００２１')).toBe(
+    expect(convertZipCode('７３２ー００２１')).toBe(
       expectedZipCodeExcludingHyphen
     );
   });
 
   test('半角のハイフンを削除', () => {
-    expect(goqZipCode.convertZipCode('732-0021')).toBe(
+    expect(convertZipCode('732-0021')).toBe(
       expectedZipCodeExcludingHyphen
     );
   });
 
   test('半角と全角の郵便番号を半角に変換', () => {
-    expect(goqZipCode.convertZipCode('７３2００２1')).toBe(
+    expect(convertZipCode('７３2００２1')).toBe(
       expectedZipCodeExcludingHyphen
     );
   });
