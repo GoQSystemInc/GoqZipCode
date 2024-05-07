e7f7a1c646218c113dcf03c24b6e13bc180c154a
marumoto <marumoto@goq.co.jp>
2024-05-07 11:11:10 +0900

    refactor:テストケースが一つのためdescrive削除

--- src/test/utils/convertHyphenatedZipCode.test.ts
+++ src/test/utils/convertHyphenatedZipCode.test.ts
@@ -1,8 +1,6 @@
 import { expect } from '@jest/globals';
 import { goqZipCode } from '../goqZipCode';
 
-describe('オプションによってハイフンを付与', () => {
-  test('オプションでハイフンありを指定している場合、郵便番号にハイフンを追加する', () => {
-    expect(goqZipCode.convertHyphenatedZipCode('7340001')).toBe('734-0001');
-  });
+test('オプションでハイフンありを指定している場合、郵便番号にハイフンを追加する', () => {
+  expect(goqZipCode.convertHyphenatedZipCode('7340001')).toBe('734-0001');
 });
