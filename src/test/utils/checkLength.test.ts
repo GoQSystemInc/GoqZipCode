6c5c74bd86b49686e3e57d0757972423347d5470
marumoto <marumoto@goq.co.jp>
2024-05-15 09:58:19 +0900

    文字数の長さを検証するテストのパターンを増やしました

--- src/test/utils/checkLength.test.ts
+++ src/test/utils/checkLength.test.ts
@@ -5,24 +5,24 @@ describe('郵便番号が期待する桁数かチェック', () => {
   test('完全一致検索で入力データが7文字ならtrueを返す', () => {
     expect(checkLength(true, 7)).toBe(true);
   });
-  
-  test('完全一致検索で入力データが6文字以下ならfalseを返す', () => {
-    expect(checkLength(true, 6)).toBe(false);
-  });
 
-  test('完全一致検索で入力データが0文字ならfalseを返す', () => {
+  test('完全一致検索で入力データが7文字以外ならfalseを返す', () => {
     expect(checkLength(true, 0)).toBe(false);
-  });
-
-  test('完全一致検索で入力データが7文字以上ならfalseを返す', () => {
+    expect(checkLength(true, 2)).toBe(false);
+    expect(checkLength(true, 4)).toBe(false);
     expect(checkLength(true, 8)).toBe(false);
+    expect(checkLength(true, 10)).toBe(false);
   });
 
   test('部分一致検索で入力データが2文字以上ならtrueを返す', () => {
     expect(checkLength(false, 2)).toBe(true);
+    expect(checkLength(false, 4)).toBe(true);
+    expect(checkLength(false, 8)).toBe(true);
+    expect(checkLength(false, 10)).toBe(true);
   });
 
   test('部分一致検索で入力データが1文字以下の場合はfalseを返す', () => {
+    expect(checkLength(false, 0)).toBe(false);
     expect(checkLength(false, 1)).toBe(false);
   });
 });
