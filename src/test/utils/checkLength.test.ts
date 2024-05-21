c74f2ef15c19082e378a6921b23204a0a454f026
marumoto <marumoto@goq.co.jp>
2024-05-21 13:42:58 +0900

    条件が2桁以下の時の同値パーテーション以外のテストケースを削除

--- src/test/utils/checkLength.test.ts
+++ src/test/utils/checkLength.test.ts
@@ -17,7 +17,6 @@ describe('完全一致検索時の桁数はチェック', () => {
   });
 
   test('部分一致検索で入力データが1文字以下の場合はfalseを返す', () => {
-    expect(checkLength(false, 0)).toBe(false);
     expect(checkLength(false, 1)).toBe(false);
   });
 });
