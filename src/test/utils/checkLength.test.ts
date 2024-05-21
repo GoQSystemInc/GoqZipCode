7e7aa8eefa02ce3c94aefecba2488028dbf75bdb
marumoto <marumoto@goq.co.jp>
2024-05-21 13:41:40 +0900

    条件が2文字以上の時の同値パーテーション以外のテストケースを削除

--- src/test/utils/checkLength.test.ts
+++ src/test/utils/checkLength.test.ts
@@ -14,9 +14,6 @@ describe('完全一致検索時の桁数はチェック', () => {
   test('部分一致検索で入力データが2文字以上ならtrueを返す', () => {
     expect(checkLength(false, 2)).toBe(true);
     expect(checkLength(false, 3)).toBe(true);
-    expect(checkLength(false, 4)).toBe(true);
-    expect(checkLength(false, 8)).toBe(true);
-    expect(checkLength(false, 10)).toBe(true);
   });
 
   test('部分一致検索で入力データが1文字以下の場合はfalseを返す', () => {
