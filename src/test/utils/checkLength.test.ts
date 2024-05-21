223a127e6492b9c647836c5fe2564aad6a4acd0d
marumoto <marumoto@goq.co.jp>
2024-05-21 13:39:55 +0900

    条件が7桁以上の時の同値パーテーション以外のテストケースを削除

--- src/test/utils/checkLength.test.ts
+++ src/test/utils/checkLength.test.ts
@@ -7,12 +7,8 @@ describe('完全一致検索時の桁数はチェック', () => {
   });
 
   test('完全一致検索で入力データが7文字以外ならfalseを返す', () => {
-    expect(checkLength(true, 0)).toBe(false);
-    expect(checkLength(true, 2)).toBe(false);
-    expect(checkLength(true, 4)).toBe(false);
     expect(checkLength(true, 6)).toBe(false);
     expect(checkLength(true, 8)).toBe(false);
-    expect(checkLength(true, 10)).toBe(false);
   });
 
   test('部分一致検索で入力データが2文字以上ならtrueを返す', () => {
